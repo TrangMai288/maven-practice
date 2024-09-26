1b5c3126329042e6e7519aa7cbc43ad52f8532f3
TrangMai288 <trangmai828@gmail.com>
2024-09-26 23:21:13 +0700

    practice buoi 1

---
+++ src/main/java/org/example/practice1/EvenOdd.java
@@ -0,0 +1,10 @@
+package org.example.practice1;
+
+public class EvenOdd {
+    /**
+     * Tim so chan, so le
+     */
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ src/test/java/org/example/practice1/PasswordComplexityTest.java
@@ -0,0 +1,48 @@
+package org.example.practice1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PasswordComplexityTest {
+    // nhap du 8 ky tu, co so, co ky tu thuong, co ky tu hoa, !@#$%^&*()
+    @Test
+    void verify_Trang123456a$_is_password() {
+        Assert.assertTrue(PasswordComplexity.isPasswordComplexity("Trang123456a$"));
+    }
+
+    // nhap khong du 8 ky tu
+    @Test
+    void verify_Trang1$_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("Trang1$"));
+    }
+
+    // nhap 8 ky tu
+    @Test
+    void verify_Trang12$_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("Trang12$"));
+    }
+
+    // nhap khong co so
+    @Test
+    void verify_Trangtester$_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("Trangtester$"));
+    }
+
+    // nhap khong co chu thuong
+    @Test
+    void verify_TRANG123$_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("TRANG123$"));
+    }
+
+    // nhap khong co chu hoa
+    @Test
+    void verify_trangtest1$_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("trangtest1$"));
+    }
+
+    // nhap khong co ky tu dac biet
+    @Test
+    void verify_Trang123456_is_not_password() {
+        Assert.assertFalse(PasswordComplexity.isPasswordComplexity("Trang123456"));
+    }
+}
