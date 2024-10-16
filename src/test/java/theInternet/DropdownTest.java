fecb3d60f792b94e5916f31678207ed4906ed46b
TrangMai288 <trangmai828@gmail.com>
2024-10-16 23:35:31 +0700

    practice buoi 4 lan 2

--- src/test/java/theInternet/DropdownTest.java
+++ src/test/java/theInternet/DropdownTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,31 +19,50 @@ public class DropdownTest {
      4. Validate "option 1" is selected
      */
     WebDriver driver;
+
     @BeforeMethod
     void setUp() {
         driver = new ChromeDriver();
     }
 
+    // cach 1
     @Test
-    void dropDown() {
+    void dropDownTest1() {
         driver.get("https://the-internet.herokuapp.com/dropdown");
-
-        // cach 1
-        WebElement dropdown1 = driver.findElement(By.xpath("//select[@id='dropdown']/option[contains(.,'Option 1')]"));
+        WebElement dropdown1 = driver.findElement(By.xpath("//select[@id='dropdown']/option[@value='1']"));
         dropdown1.click();
         Assert.assertTrue(dropdown1.isSelected());
+    }
 
-        // cach 2
+    // cach 2
+    @Test
+    void dropdownTest2() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        Select dropdown = new Select(driver.findElement(By.id("dropdown")));
+        dropdown.selectByValue("1");
+        Assert.assertTrue(dropdown.getFirstSelectedOption().isSelected());
+    }
+
+    // cach 3
+    @Test
+    void dropdownTest3() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
         Select dropdown = new Select(driver.findElement(By.id("dropdown")));
         dropdown.selectByIndex(1);
-        Assert.assertTrue(dropdown1.isSelected());
+        Assert.assertEquals(dropdown.getFirstSelectedOption().getText(), "Option 1");
+    }
 
-        // cach 3
-        dropdown.selectByValue("1");
-        Assert.assertTrue(dropdown1.isSelected());
-
-        // cach 4
+    // cach 4
+    @Test
+    void dropdownTest4() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        Select dropdown = new Select(driver.findElement(By.id("dropdown")));
         dropdown.selectByVisibleText("Option 1");
-        Assert.assertTrue(dropdown1.isSelected());
+        Assert.assertEquals(dropdown.getOptions().get(1).getText(), "Option 1");
+    }
+
+    @AfterMethod
+    void quit() {
+        driver.quit();
     }
 }
